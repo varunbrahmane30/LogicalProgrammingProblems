e6fa924abc51d22dbf390fc62007a82e5fbadd70
name <varunbrahmane30@gmail.com>
2021-07-07 16:01:57 +0530

    [varun] added: Generate Febonacci series

---
+++ FebonacciSeries.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D6LogicalProblems
+{
+    class FebonacciSeries
+    {
+        public static void calFebonacciSeries()
+        {
+			// variables
+			int a = 0;
+			int b = 1;
+			int c = 0;
+			
+
+			// taking user input 
+            Console.WriteLine("How many Iteration do you want: ");
+			int input =  int.Parse(Console.ReadLine());
+
+            //implementation
+            Console.WriteLine("Febonacci Series : ");
+			for (int i = 1; i <= input; i++)
+			{
+				Console.Write(a + "  ");
+				c = a + b;
+				a = b;
+				b = c;
+			}
+		}
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace D6LogicalProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Logical Programs");
+            FebonacciSeries.calFebonacciSeries();
+            Console.ReadKey();
         }
     }
 }
