7b091f7208ded8a1cf611a40b550355357e80d8e
name <varunbrahmane30@gmail.com>
2021-07-07 15:42:12 +0530

    [varun] added: logical programs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace D6LogicalProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logical Programs");
+        }
+    }
+}
